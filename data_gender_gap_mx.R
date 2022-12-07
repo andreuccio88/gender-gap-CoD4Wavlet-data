d1524c69198703f2d7fc96a43998d95c0725cf81
andreuccio88 <45309313+andreuccio88@users.noreply.github.com>
2022-12-07 17:12:31 +0100

    ok

--- data_gender_gap_mx.R
+++ data_gender_gap_mx.R
@@ -1,45 +1,30 @@
-# 1-males, 2-females, 3-both sexes
+###############################################################################
+#                                                                             #
+# Script per ottenere geneder gap per cause di morte.                         #
+# Dati cause di morte da https://www.causesofdeath.org/                       #
+#                                                                             #
+# 1-males, 2-females, 3-both sexes                                            #
+# Files with age-specific death rates                                         #
+# Death rates are calculated per 1,000,000 and published as integer numbers.  #
+#                                                                             #
+###############################################################################
+
 
 ########################
 #                      #
-# RATES                #
+# RATES male           #
 #                      #
 ########################
+
+library(tidyverse)
+library(reshape)
+library(data.table)
 
 
 usa <- read.csv("USA_m_short_idr.csv",header = T)
 head(usa)
 
-#Files with age-specific death rates
-#Files with age-specific death rates have same structure as death files. Death rates are calculated per
-#1,000,000 and published as integer numbers.
-51
-# 
-# usa <- read.csv("USA_d_short_idr.csv",header = T)
-# head(usa)
-# 
-# 89.92/1761647*(1000000)
-# 
-# usa <- read.csv("USA_e.csv",header = T)
-# head(usa)
-# 253/1000000
-# 
-# 445.89/1761647*(1000000)
-
-str(usa)
-library(tidyverse)
-library(reshape)
-
-usa_M <- usa %>% filter(sex==1,cause!=0)
-usa_M$country
-
-head(usa_M)
-str(usa_M)
-
-head(usa_M[,2])
-
-head(usa_M[,c(1,2,3,4,5,6,7,25,26,27,28,29,30,31)])
-
+usa_M <- usa %>% filter(sex==1,cause!=0) # select 1-male
 usa_M <- usa_M[,-c(1,3,4,5,25,27,29)]
 
 head(usa_M)
@@ -52,10 +37,8 @@ usa_M <- melt(usa_M, id.vars =c("cause","year"), measure.vars = c("m0", "m1", "m
                                                               "m95","m100p"))
 
 
-head(usa_M)
+# ricodifica eta
 usa_M$Age <- NA
-
-
 usa_M$Age[usa_M$variable=="m0"] <- 0
 usa_M$Age[usa_M$variable=="m1"] <- 1
 usa_M$Age[usa_M$variable=="m5"] <- 5
@@ -79,101 +62,62 @@ usa_M$Age[usa_M$variable=="m90"] <- 90
 usa_M$Age[usa_M$variable=="m95"] <- 95
 usa_M$Age[usa_M$variable=="m100p"] <- 100
 
-str(usa_M)
-
-usa$cause
-library(data.table)
 
 usa_M <- as.data.table(usa_M)
 
 head(usa_M)
 names(usa_M)[4]<-"mx.tot.by.Caus"
-#names(usa)[2]<-"Year"
 
-head(usa_M)
 
+# Classificazione cause
 usa_M$Cause_Rev[usa_M$cause==1] <- 1 # inf
-
 usa_M$Cause_Rev[usa_M$cause==2] <- 2 # neop
-
 usa_M$Cause_Rev[usa_M$cause==3] <- 7 # oth
 usa_M$Cause_Rev[usa_M$cause==4] <- 7 # oth
 usa_M$Cause_Rev[usa_M$cause==5] <- 7 # oth
 usa_M$Cause_Rev[usa_M$cause==6] <- 7 # oth
-
 usa_M$Cause_Rev[usa_M$cause==7] <- 3 # CVD
 usa_M$Cause_Rev[usa_M$cause==8] <- 3 # CVD
 usa_M$Cause_Rev[usa_M$cause==9] <- 3 # CVD
-
 usa_M$Cause_Rev[usa_M$cause==10] <- 4 # RESP
 usa_M$Cause_Rev[usa_M$cause==11] <- 4 # RESP
-
 usa_M$Cause_Rev[usa_M$cause==12] <- 5 # DIG
-
 usa_M$Cause_Rev[usa_M$cause==13] <- 7 # oth
 usa_M$Cause_Rev[usa_M$cause==14] <- 7 # oth
 usa_M$Cause_Rev[usa_M$cause==15] <- 7 # oth
-
 usa_M$Cause_Rev[usa_M$cause==16] <- 6 # ext
-#usa_M$Age <- as.character(usa_M$Age)
-head(usa_M)
 
 usa_M$mx.tot.by.Caus <- usa_M$mx.tot.by.Caus/1000000
-
 usa_M %>%filter(year==1979,Age%in%c(0),cause%in%c(7,8,9)) %>%select(mx.tot.by.Caus) %>%  sum()
 
-str(usa)
 usa_M=usa_M[,.(mx.tot.by.Caus = sum(mx.tot.by.Caus)), keyby = .(year, Cause_Rev, Age)]
-head(usa_M)
 usa_M %>% filter(Cause_Rev==3)
-
 usa_M %>%filter(year==1979) %>% ggplot(aes(Age,log(mx.tot.by.Caus)))+geom_line()+facet_wrap(~Cause_Rev)
 
 usa_M$Sex <- "M"
-
-
 save(usa_M,file="usa_M.RData")
 ############################################################
 
-rm()
-# 1-males, 2-females, 3-both sexes
 
-usa <- read.csv("USA_m_short_idr.csv",header = T)
-head(usa)
 
-#Files with age-specific death rates
-#Files with age-specific death rates have same structure as death files. Death rates are calculated per
-#1,000,000 and published as integer numbers.
-51
-# 
-# usa <- read.csv("USA_d_short_idr.csv",header = T)
-# head(usa)
-# 
-# 89.92/1761647*(1000000)
-# 
-# usa <- read.csv("USA_e.csv",header = T)
-# head(usa)
-# 253/1000000
-# 
-# 445.89/1761647*(1000000)
 
-str(usa)
+
+
+rm(list=ls())
+########################
+#                      #
+# RATES female         #
+#                      #
+########################
+
 library(tidyverse)
 library(reshape)
+library(data.table)
 
-usa_F <- usa %>% filter(sex==2,cause!=0)
-usa_F$country
 
-head(usa_F)
-str(usa_F)
-
-head(usa_F[,2])
-
-head(usa_F[,c(1,2,3,4,5,6,7,25,26,27,28,29,30,31)])
-
+usa <- read.csv("USA_m_short_idr.csv",header = T)
+usa_F <- usa %>% filter(sex==2,cause!=0) #select 2-female
 usa_F <- usa_F[,-c(1,3,4,5,25,27,29)]
-
-head(usa_F)
 
 usa_F <- melt(usa_F, id.vars =c("cause","year"), measure.vars = c("m0", "m1", "m5", "m10", 
                                                                   "m15", "m20", "m25", "m30", 
@@ -183,10 +127,9 @@ usa_F <- melt(usa_F, id.vars =c("cause","year"), measure.vars = c("m0", "m1", "m
                                                                   "m95","m100p"))
 
 
-head(usa_F)
 usa_F$Age <- NA
 
-
+# ricodifica eta
 usa_F$Age[usa_F$variable=="m0"] <- 0
 usa_F$Age[usa_F$variable=="m1"] <- 1
 usa_F$Age[usa_F$variable=="m5"] <- 5
@@ -210,62 +153,46 @@ usa_F$Age[usa_F$variable=="m90"] <- 90
 usa_F$Age[usa_F$variable=="m95"] <- 95
 usa_F$Age[usa_F$variable=="m100p"] <- 100
 
-str(usa_F)
-
-usa$cause
-library(data.table)
-
 usa_F <- as.data.table(usa_F)
 
-head(usa_F)
 names(usa_F)[4]<-"mx.tot.by.Caus"
-#names(usa)[2]<-"Year"
 
-head(usa_F)
-
+# Classificazione cause
 usa_F$Cause_Rev[usa_F$cause==1] <- 1 # inf
-
 usa_F$Cause_Rev[usa_F$cause==2] <- 2 # neop
-
 usa_F$Cause_Rev[usa_F$cause==3] <- 7 # oth
 usa_F$Cause_Rev[usa_F$cause==4] <- 7 # oth
 usa_F$Cause_Rev[usa_F$cause==5] <- 7 # oth
 usa_F$Cause_Rev[usa_F$cause==6] <- 7 # oth
-
 usa_F$Cause_Rev[usa_F$cause==7] <- 3 # CVD
 usa_F$Cause_Rev[usa_F$cause==8] <- 3 # CVD
 usa_F$Cause_Rev[usa_F$cause==9] <- 3 # CVD
-
 usa_F$Cause_Rev[usa_F$cause==10] <- 4 # RESP
 usa_F$Cause_Rev[usa_F$cause==11] <- 4 # RESP
-
 usa_F$Cause_Rev[usa_F$cause==12] <- 5 # DIG
-
 usa_F$Cause_Rev[usa_F$cause==13] <- 7 # oth
 usa_F$Cause_Rev[usa_F$cause==14] <- 7 # oth
 usa_F$Cause_Rev[usa_F$cause==15] <- 7 # oth
-
 usa_F$Cause_Rev[usa_F$cause==16] <- 6 # ext
-#usa_F$Age <- as.character(usa_F$Age)
-head(usa_F)
 
 usa_F$mx.tot.by.Caus <- usa_F$mx.tot.by.Caus/1000000
 
 usa_F %>%filter(year==1979,Age%in%c(0),cause%in%c(7,8,9)) %>%select(mx.tot.by.Caus) %>%  sum()
-
-str(usa)
 usa_F=usa_F[,.(mx.tot.by.Caus = sum(mx.tot.by.Caus)), keyby = .(year, Cause_Rev, Age)]
-head(usa_F)
 usa_F %>% filter(Cause_Rev==3)
-
 usa_F %>%filter(year==1979) %>% ggplot(aes(Age,log(mx.tot.by.Caus)))+geom_line()+facet_wrap(~Cause_Rev)
-
 usa_F$Sex <- "F"
 
 
+
+####################
+#
+# Merge data M & F
+#
+####################
+
 load("usa_M.RData")
 
-usa_M
 
 names(usa_F)[4]<-"mx.tot.by.Caus_F"
 head(usa_F)
@@ -273,15 +200,8 @@ head(usa_F)
 tot <- cbind(usa_F,usa_M)
 
 tot$gender_gap <- tot$mx.tot.by.Caus/tot$mx.tot.by.Caus_F 
-
-
-head(tot)
 tot <- tot[,c(1,2,3,11)]
 tot %>%filter(year==2000) %>% ggplot(aes(Age,(gender_gap)))+geom_line()+facet_wrap(~Cause_Rev,scales = "free")
-
-summary(tot$gender_gap)
-
-head(tot)
 tot$Cohort <- tot$year-tot$Age
   
 tot %>%filter(Age==25) %>% ggplot(aes(Cohort,(gender_gap)))+geom_line()+
